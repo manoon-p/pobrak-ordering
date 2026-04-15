2263358038917d61070228fd255b77ab3fead30e
manoon.p <manoon.p@gmail.com>
2026-04-15 09:32:31 +0700

    add staff page

---
+++ app/staff/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function StaffPage() {
+  return (
+    <div style={{fontFamily:"sans-serif",padding:"20px",background:"#1b4332",minHeight:"100vh",color:"white"}}>
+      <h1>🖥️ Staff Dashboard</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
